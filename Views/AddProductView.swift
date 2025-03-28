c884d17ce15896f6d505e3d9ed066d6e67c7315b
Adam Simcoe <adamsimcoe@Adams-Mac-mini.local>
2025-03-28 16:08:08 -0400

    Added comments to AddProductView

--- Views/AddProductView.swift
+++ Views/AddProductView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct AddProductView: View {
     
+    // Accesses core data context for creating and saving new product
     @Environment(\.managedObjectContext) var context
+    
+    // Dismisses view when form has been submitted
     @Environment(\.dismiss) var dismiss
     
+    // State vars for form inputs
     @State private var productName = ""
     @State private var productDescription = ""
     @State private var productPrice = ""
@@ -21,29 +25,40 @@ struct AddProductView: View {
         
         Form {
             
+            // Text input for product name
             TextField("Product Name", text: $productName)
             
+            // Text input for product description
             TextField("Product Description", text: $productDescription)
             
+            // Text input for product price, only accepts decimal values
             TextField("Product Price", text: $productPrice)
                 .keyboardType(.decimalPad)
             
+            // Text input for product provider
             TextField("Product Provider", text: $productProvider)
             
+            // Save product button
             Button("Save Product") {
                 
+                // Creates new product entity
                 let newProduct = Product(context: context)
                 
+                // Assigns form data to new product entity
                 newProduct.id = UUID()
                 newProduct.productName = productName
                 newProduct.productDescription = productDescription
                 newProduct.productPrice = Double(productPrice) ?? 0.0
                 newProduct.productProvider = productProvider
                 
+                // Saves new product
                 try? context.save()
+                
+                // Dismisses form view
                 dismiss()
             }
         }
+        // Navigation bar title
         .navigationTitle("Add a Product")
     }
 }
