b23b8aba0a7f99d2cd53ab9fde9717d45f178f76
Adam Simcoe <adamsimcoe@Adams-Mac-mini.local>
2025-03-28 16:20:24 -0400

    Added comments to ProductListView

--- Views/ProductListView.swift
+++ Views/ProductListView.swift
@@ -9,24 +9,30 @@ import SwiftUI
 
 struct ProductListView: View {
     
+    // Fetch all products from Core Data, sort them based on Product Name in ascending order
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Product.productName, ascending: true)]
     ) var products: FetchedResults<Product>
     
     var body: some View {
         
+        // List display of all products, using ID as the identifier
         List(products, id: \.id) { product in
             
+            // Vertical stack, aligns product name/description to the left
             VStack(alignment: .leading) {
                 
+                // Product name text display
                 Text(product.productName ?? "Product name unavailable")
                     .font(.headline)
                 
+                // Product description text display
                 Text(product.productDescription ?? "Product description unavailable")
                     .font(.subheadline)
             }
         }
         
+        // Navigation bar title
         .navigationTitle("All Products List")
     }
 }
