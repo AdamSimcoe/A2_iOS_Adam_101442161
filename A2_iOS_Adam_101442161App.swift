b466e77d25a1b5e61937ff73bf4732c3e1f4da60
Adam Simcoe <adamsimcoe@Adams-Mac-mini.local>
2025-03-28 16:32:39 -0400

    Added comments to main app entry

--- A2_iOS_Adam_101442161App.swift
+++ A2_iOS_Adam_101442161App.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 @main
 struct A2_iOS_Adam_101442161App: App {
+    
+    // shared persistence instance to access Core Data
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
+            // Navigation View for NavigationLink functionality
             NavigationView {
                 ProductMainView()
             }
             
+            // Core Data Context injection to be used by other views
             .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
